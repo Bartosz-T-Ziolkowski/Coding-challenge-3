cab29382559d0467bc9968d5fee8f88b5aeb79e3
Bartosz <bziolkowski@usf.edu>
2025-09-14 00:47:36 -0400

    initial coding challenege upload to github

---
+++ cc_3.js
@@ -0,0 +1,24 @@
+// Coding Challenge 00
+
+// Code goes here
+
+let customers = [
+    {
+        name: "Jack",
+        email: "Jack@example.com",
+        purchases: ""
+    },
+    {
+        name: "Mary",
+        email: "Mary@example.com",
+        purchases: "",
+    },
+    {
+        name: "Sue",
+        email: "Sue@example.com",
+        purchases: ["apples","oranges"],
+    }
+]
+console.log(customer1)
+console.log(customer2)
+console.log(customer3)
