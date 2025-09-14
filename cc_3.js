ddd4495da585a5553be3aba45a2fa7bfce977cd2
Bartosz <bziolkowski@usf.edu>
2025-09-14 02:24:09 -0400

    new commit with most of the code done

--- cc_3.js
+++ cc_3.js
@@ -1,24 +1,44 @@
-// Coding Challenge 00
+// Coding Challenge 3a
 
 // Code goes here
+
+// Step 2: Create customer records
 
 let customers = [
     {
         name: "Jack",
         email: "Jack@example.com",
-        purchases: ""
+        purchases: ["pens", "pencils", "erasers"]
     },
     {
         name: "Mary",
         email: "Mary@example.com",
-        purchases: "",
+        purchases: ["cups", "plates", "spoons"] 
     },
     {
         name: "Sue",
         email: "Sue@example.com",
-        purchases: ["apples","oranges"],
+        purchases: ["apples","oranges", "mangos"],
     }
 ]
-console.log(customer1)
-console.log(customer2)
-console.log(customer3)
+// Current customer summary
+
+customers.forEach(obj =>console.log(`name: ${obj.name} | email: ${obj.email} | purchases: ${obj.purchases}`));
+
+// Step 3: Add and remove data
+
+newObject = {name:"Raiden", email:"Raiden@example.com", purchases:["boxes", "spy gear", "carbon fiber armor"]};
+customers.push(newObject);
+customers.shift();
+console.log(customers);
+
+// Update customer info
+
+customers[1]["email"] = "Sueanne@example.com";
+console.log(customers);
+customers[1].purchases.push("forks");
+console.log("Mary's purchases: " + customers[1].purchases);
+
+//
+
+customers.forEach(obj =>console.log(`name: ${obj.name} | email: ${obj.email} | purchases: ${obj.purchases.length}`));
